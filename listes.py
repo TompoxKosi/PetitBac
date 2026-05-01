bad2e65dd9783d1b65189bbc6ca4e33774a7c88e
TompoxKosi <codenamekfem@gmail.com>
2026-05-01 18:20:48 +0200

    Create listes.py

---
+++ listes.py
@@ -0,0 +1,46 @@
+liste_1 = [
+            "Villes : ", 
+            "Monuments et sites historiques ou naturelles : ", 
+            "Cours/étendues d'eau (rivières, lacs, mers, ...) : ", 
+            "Animaux : ", 
+            "Plantes et champignons : ", 
+            "Métiers : ",
+            "Couleurs et teintes : ", 
+            "Plats cuisinés : ", 
+            "Boissons (pas de marque) : ",
+            "Fromages et épices : ", 
+            "Prénoms : ", 
+            "Célébrités (nom de famille/pseudo) : ",
+            "Personnages fictifs : ", 
+            "Marques : ", 
+            "Matériaux : ", 
+            "Véhicules et moyens de transport : ", 
+            "Vêtements et accessoires : ",
+            "Sports : ",
+            "Titres de films/série (web/audio)/émission : ",
+            "Titres de roman/BD/pièce de théâtre : ", 
+            "Titres de musique/album : ", 
+            "Titres de jeu vidéo : "
+            ]
+liste_2 = [
+            "Choses qui vont par pair : ", 
+            "Choses qui tiennet dans la main : ", 
+            "Choses qui rentrent dans autre chose  : ",
+            "Choses qui sont recouvertes de quelque chose : ", 
+            "Choses qui se plient : ", 
+            "Choses qui s'empilent : ", 
+            "Choses avec un ou des pieds : ",
+            "Choses avec un ou des trous", 
+            "Choses à la verticale : ", 
+            "Choses à l'horizontal : "
+            ]
+liste_3 = [
+            "Catégories X : ", 
+            "Titres de films nuls (inventés) : ", 
+            "Motifs de rupture : ", 
+            "Noms de chat : ", 
+            "Noms de drag queen : ",
+            "Synonymes de 'pénis' (existants ou inventés) : ", 
+            "Insultes : ", 
+            "Mots rares ou bizarres : "
+            ]
